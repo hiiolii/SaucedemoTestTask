e9cc4a2f031dddc81c29ad3042daf67ed8a408e6
hiiolii <serhiioliinykqa@gmail.com>
2025-06-07 23:15:29 +0300

    Login mobile

---
+++ tests/LoginMobile.spec.js
@@ -0,0 +1,16 @@
+import { test, expect, devices } from '@playwright/test';
+
+test.use({
+  ...devices['iPhone 13'],
+});
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await expect(page.locator('[data-test="title"]')).toContainText('Products');
+});
